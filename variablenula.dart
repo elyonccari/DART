4b62c42c607769b2139657d0b5521332d8653c03
elyonccari <elyonccari7692@gmail.com>
2023-08-23 08:09:50 -0500

    variablenula

--- variablenula.dart
+++ variablenula.dart
@@ -1,5 +1,16 @@
 void main(List<String> args) {
   //nullsafety
-  String nombre = "Elyon";
-  print(nombre);
+  String? nombre;
+  nombre = "Elyon";
+  print(nombre.toUpperCase());
+
+  int? numero;
+  numero = 5;
+  int total = numero! + 5;
+  print(total);
+
+  String? apellido;
+  apellido = "ccari";
+  String apellido1 = apellido??"sin apellido";
+  print(apellido1);
 }
