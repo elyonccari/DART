b2c6427183b3a6a217eb6b7e055a3f3298b2ba2f
elyonccari <elyonccari7692@gmail.com>
2023-08-24 12:39:24 -0500

    trabajo en casa clases

---
+++ trabajo en casa clases.dart
@@ -0,0 +1,13 @@
+void main(){
+final hero wolverine = hero('logan', 'Regeneracion');
+print(wolverine);
+print(wolverine.name);
+print(wolverine.power);
+}
+class hero{
+  String name;
+  String power;
+
+hero(this.name, this.power);
+
+}
